78308aaa7c7925942ef5c653ba11d942b181dff1
Ajay Dandge <60701544+er-knight@users.noreply.github.com>
2021-06-04 21:48:00 +0530

    .

---
+++ bookman.py
@@ -0,0 +1,12 @@
+def bookman(url: str):
+    """ bookman means bookmarks manager. """
+
+    with open("bookmarks.txt", "a") as bookmarks:
+        bookmarks.write(f"{url}\n")
+    
+    print("bookmarks updated!")
+
+if __name__ == "__main__":
+    
+    url = input("link : ")
+    bookman(url)
